d9fa069a5c29e6333bcc9aecfe931767d008cb71
dwhatley <dwhatley@mit.edu>
2017-04-29 23:35:33 -0400

    fixed constraints to consider OARs and affected voxels

--- optimization.py
+++ optimization.py
@@ -2,7 +2,15 @@ import scipy.io as io
 import numpy as np
 from gurobipy import *
 
-# mat_contents = io.loadmat('TG119/Body_VOILIST.mat')
+start = 1600000
+end = 1601000
+
+voi_contents = io.loadmat('TG119/Body_VOILIST.mat')
+voxels = {}
+
+for x in voi_contents['v']:
+	if(x[0] > start and x[0] <= end):
+		voxels[x[0]-start] = True
 
 # solving the problem for pre-set gantry and couch angles
 # uncomment lines as needed
@@ -11,7 +19,7 @@ mat_contents = io.loadmat('TG119/Gantry0_Couch0_D.mat')
 # mat_contents = io.loadmat('TG119/Gantry144_Couch0_D.mat')
 # mat_contents = io.loadmat('TG119/Gantry216_Couch0_D.mat')
 # mat_contents = io.loadmat('TG119/Gantry288_Couch0_D.mat')
-f = open('00.txt', 'w')
+# f = open('00.txt', 'w')
 
 # sz = mat_contents['v'].size
 # for i in range(sz):
@@ -23,7 +31,9 @@ f = open('00.txt', 'w')
 
 # ACTUAL OPTIMIZATION CODE STARTS ON LINE 30
 
+#uncomment this line too
 x = mat_contents['D'].shape
+
 # doses = []
 # for i in range(1600000, 1601000):
 #   if(mat_contents['D'][i].size == 0): continue
@@ -40,32 +50,40 @@ x = mat_contents['D'].shape
 #   but that'll induce some suboptimality"
 # 
 
-start = 1600000
-end = 1601000
+# UNCOMMENT THIS CODE WHEN OPTIMIZING
 rows = end - start
 
 m = Model("dose influence")
 # variables, constraint 5
 fluence = [0] * x[1]
 for i in range(x[1]):
-    fluence[i] = m.addVar(lb=0.01, name=('x'+str(i)))
+    fluence[i] = m.addVar(lb=0, name=('x'+str(i)))
 
 # constraint 2, 3, 4
 d = [0] * (end - start)
 for i in range(start, end):
-    d[i - start] = m.addVar(lb=0, ub=1, name=('d'+str(i)))
+    d[i - start] = m.addVar(lb=0, ub=100, name=('d'+str(i)))
     arr = mat_contents['D'].getrow(i).toarray()
     m.addConstr(quicksum(arr[0,j] * fluence[j] for j in range(x[1])) == d[i - start])
 
 # constraint 6 not needed
 # because we are pre-setting the beam angle
 
-# constraint 7, 8 not implemented yet
-# no idea what y_h means
+# constraint 7
+ub_ntumor = 75
+for i in range(start, end):
+	if(i - start not in voxels):
+		m.addConstr(ub_ntumor >= d[i - start])
+
+# constraint 8 not needed
+# because not implementing constraint 1
 
 # objective function
 # leave alpha at 0, no need to consider y's
-m.setObjective(sum(d), GRB.MINIMIZE)
+obj = 0
+for i in voxels:
+	obj += d[i]
+m.setObjective(obj, GRB.MAXIMIZE)
 
 m.update()
 m.write("debug.lp")
