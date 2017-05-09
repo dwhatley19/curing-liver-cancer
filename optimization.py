3312e58cd8e2b35148876b0ebf161ef123e132a2
dwhatley <dwhatley@mit.edu>
2017-05-09 00:38:09 -0400

    optimized code, need to fix some constraints

--- optimization.py
+++ optimization.py
@@ -1,9 +1,11 @@
 import scipy.io as io
 import numpy as np
+import thread
+import time
 from gurobipy import *
 
-start = 1519000
-end = 1520000
+start = 1500000
+end = 1600000
 
 oar_contents = io.loadmat('TG119/Core_VOILIST.mat')
 oar_voxels = {}
@@ -66,16 +68,31 @@ rows = end - start
 
 m = Model("dose influence")
 # variables, constraint 5
-fluence = [0] * x[1]
+flu = [0] * x[1]
 for i in range(x[1]):
-    fluence[i] = m.addVar(lb=0, ub=2500, name=('x'+str(i)))
+    flu[i] = m.addVar(lb=0, ub=2500, name=('x'+str(i)))
+fluence = np.array(flu)
 
 # constraint 2, 3, 4
-d = [0] * (end - start)
-for i in range(start, end):
-    d[i - start] = m.addVar(lb=0, ub=1, name=('d'+str(i)))
-    arr = mat_contents['D'].getrow(i).toarray()
-    m.addConstr(quicksum(arr[0,j] * fluence[j] for j in range(x[1])) == d[i - start])
+arr = np.array(mat_contents['D'][start:end].todense())
+nonzero = (np.sum(arr, axis=1) > 0).astype(int)
+indices = np.cumsum(nonzero) - 1
+arr = arr[np.sum(arr, axis=1) > 0]
+d = [0] * (arr.shape[0])
+for i in range(arr.shape[0]):
+    d[i] = m.addVar(lb=0, ub=1, name=('d'+str(i)))
+
+# def add_constrs(lb, ub):
+#     m.addConstrs((np.sum(arr[i - start] * fluence) == d[i - start] for i in range(lb, ub)))
+
+# num_threads = 1000
+# num_per_thread = (end - start) / num_threads
+# for i in range(num_threads):
+#     lb = start + i * num_per_thread
+#     thread.start_new_thread(add_constrs, (lb, lb + num_per_thread))
+
+t1 = time.time()
+m.addConstrs((np.sum(arr[i] * fluence) == d[i] for i in range(arr.shape[0])))
 
 # constraint 6 not needed
 # because we are pre-setting the beam angle
@@ -83,14 +100,15 @@ for i in range(start, end):
 # tumor region constraint
 lb_tumor = 0.65
 for i in tumor_voxels:
-    m.addConstr(d[i - start] >= lb_tumor)
+    if(nonzero[i - start]):
+        m.addConstr(d[indices[i - start]] >= lb_tumor)
 
 # OAR region constraint on total sum
-ub_OAR = 20 * frac_appl_oar
-print(ub_OAR)
+ub_OAR = 200 #* frac_appl_oar
 oar_constr = 0
 for i in oar_voxels:
-    oar_constr += d[i - start]
+    if(nonzero[i - start]):
+        oar_constr += d[indices[i - start]]
 m.addConstr(oar_constr <= ub_OAR)
 
 # constraint 8 not needed
@@ -100,7 +118,8 @@ m.addConstr(oar_constr <= ub_OAR)
 # leave alpha at 0, no need to consider y's
 obj = 0
 for i in range(start, end):
-    obj += d[i - start]
+    if(nonzero[i - start]):
+        obj += d[indices[i - start]]
 m.setObjective(obj, GRB.MAXIMIZE)
 
 m.update()
@@ -111,10 +130,22 @@ m.optimize()
 
 total = 0
 
+print(time.time() - t1)
+
 for v in m.getVars():
-    print(v.varName, v.x)
+    if(v.varName[0] == 'x'): print(v.varName, v.x)
     if(v.varName[0] == 'd'): total += v.x
 
+oar_total = 0
+for i in range(start, end):
+    if(i in oar_voxels and nonzero[i - start]):
+        oar_total += d[indices[i - start]].x
+
+tumor_total = 0
+for i in range(start, end):
+    if(i in tumor_voxels and nonzero[i - start]):
+        tumor_total += d[indices[i - start]].x
+
 print('Obj:', m.objVal)
-print('Average tumor:', m.objVal / len(tumor_voxels))
-print('Average OAR:', (total - m.objVal) / (end - start - len(oar_voxels)))
+print('Average tumor:', tumor_total / len(tumor_voxels))
+print('Average OAR:', oar_total / len(oar_voxels))
